f7796af322e109bf374c2f44d9345548d3b4d4af
Eric Mikulin <eric@picovoice.ai>
2024-04-09 18:03:18 -0700

    Basic test working

--- binding/python/test_picollm.py
+++ binding/python/test_picollm.py
@@ -14,65 +14,73 @@ import unittest
 
 import numpy as np
 
-from _picollm import Picollm, PicollmError
+from _picollm import PicoLLM
 from _util import *
 
 
 class PicollmTestCase(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls._access_key = sys.argv[1]
+        cls._model_path = sys.argv[2]
+
     def setUp(self):
-        self._picollm = Picollm(device_string="best:0", library_path=pv_library_path('../..'))
+        self._picollm = PicoLLM(
+            access_key=self._access_key,
+            model_path=self._model_path,
+            library_path=pv_library_path('../..'))
 
     def tearDown(self):
-        self._picollm.delete()
+        self._picollm.release()
 
-    def test_load_chunk(self):
-        with open(sys.argv[1], 'rb') as model_file:
-            model_loaded = False
-            while not model_loaded:
-                data = model_file.read(self._picollm.max_chunk_size)
-                if not data:
-                    break
-                model_loaded = self._picollm.load_model_chunk(data)
-            self.assertTrue(model_loaded)
+    # def test_load_chunk(self):
+    #     with open(sys.argv[1], 'rb') as model_file:
+    #         model_loaded = False
+    #         while not model_loaded:
+    #             data = model_file.read(self._picollm.max_chunk_size)
+    #             if not data:
+    #                 break
+    #             model_loaded = self._picollm.load_model_chunk(data)
+    #         self.assertTrue(model_loaded)
 
-    def test_chain_multiply(self):
-        self._picollm.load_model_file(sys.argv[1])
+    # def test_chain_multiply(self):
+    #     self._picollm.load_model_file(sys.argv[1])
 
-        _, matrix_n = self._picollm.matrix_dimensions()
-        vector = [1.0] * matrix_n
+    #     _, matrix_n = self._picollm.matrix_dimensions()
+    #     vector = [1.0] * matrix_n
 
-        result_vector = self._picollm.chain_multiply(vector)
+    #     result_vector = self._picollm.chain_multiply(vector)
 
-        self.assertEqual(len(vector), len(result_vector))
+    #     self.assertEqual(len(vector), len(result_vector))
 
     def test_version(self):
         self.assertIsInstance(self._picollm.version, str)
 
-    def test_message_stack(self):
-        relative_path = '../..'
+    # def test_message_stack(self):
+    #     relative_path = '../..'
 
-        error = None
-        try:
-            c = Picollm(device_string="invalid", library_path=pv_library_path(relative_path))
-            self.assertIsNone(c)
-        except PicollmError as e:
-            error = e.message_stack
+    #     error = None
+    #     try:
+    #         c = Picollm(device_string="invalid", library_path=pv_library_path(relative_path))
+    #         self.assertIsNone(c)
+    #     except PicollmError as e:
+    #         error = e.message_stack
 
-        self.assertIsNotNone(error)
-        self.assertGreater(len(error), 0)
+    #     self.assertIsNotNone(error)
+    #     self.assertGreater(len(error), 0)
 
-        try:
-            c = Picollm(device_string="invalid", library_path=pv_library_path(relative_path))
-            self.assertIsNone(c)
-        except PicollmError as e:
-            self.assertEqual(len(error), len(e.message_stack))
-            self.assertListEqual(list(error), list(e.message_stack))
+    #     try:
+    #         c = Picollm(device_string="invalid", library_path=pv_library_path(relative_path))
+    #         self.assertIsNone(c)
+    #     except PicollmError as e:
+    #         self.assertEqual(len(error), len(e.message_stack))
+    #         self.assertListEqual(list(error), list(e.message_stack))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("usage: test_picollm.py ${WeightFile}")
+    if len(sys.argv) != 3:
+        print("usage: test_picollm.py ${AccessKey} ${ModelFile}")
         exit(1)
 
     unittest.main(argv=sys.argv[:1])

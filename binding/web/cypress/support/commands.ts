d3d31bce1821052845ef383b0714f8a2f3d79c70
Ian <ian@picovoice.ai>
2024-04-25 14:47:13 -0700

    log

--- binding/web/cypress/support/commands.ts
+++ binding/web/cypress/support/commands.ts
@@ -1,21 +1,24 @@
-
-Cypress.Commands.add("readFiles", (files: string[]) => {
+Cypress.Commands.add('readFiles', (files: string[]) => {
   const chunks: any[] = [];
 
   for (const file of files) {
     for (let i = 0; i < 5; i++) {
       try {
+        cy.log(`Reading chunk ${file}`);
         cy.readFile(file, null, { timeout: 30000 }).then(chunk => {
+          cy.log(`Reading chunk ${file}`);
           chunks.push(new Blob([chunk]));
         });
         break;
-      } catch (e) {
+      } catch (e: any) {
+        cy.log(`${e}`);
+
         // silently fail and try again
         // eslint-disable-next-line cypress/no-unnecessary-waiting
         cy.wait(500);
       }
     }
   }
-
+  cy.log(`Reading finished`);
   cy.then(() => chunks);
 });

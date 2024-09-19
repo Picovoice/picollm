5051766e373319936ca989e261844da5b145def3
Ian Lavery <ian@picovoice.ai>
2024-09-19 15:21:56 -0700

    fix

--- binding/ios/PicoLLMAppTest/PicoLLMAppTestUITests/BaseTest.swift
+++ binding/ios/PicoLLMAppTest/PicoLLMAppTestUITests/BaseTest.swift
@@ -17,7 +17,7 @@ class BaseTest: XCTestCase {
     let accessKey: String = "{TESTING_ACCESS_KEY_HERE}"
     let modelURL = URL(string: "{TESTING_MODEL_URL_HERE}")
 
-    let cleanModel = true
+    let cleanModel = false
 
     var modelPath: String = ""
     var picollmTestData: [String: Any]?

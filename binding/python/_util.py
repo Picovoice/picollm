d7bf9b077a78416ebcc82f34ae368aca1494ccaa
Matthew Maxwell <matthew@picovoice.ai>
2025-01-08 14:56:45 -0800

    fixed python platform detect

--- binding/python/_util.py
+++ binding/python/_util.py
@@ -82,9 +82,9 @@ def pv_library_path(relative: str) -> str:
                 relative,
                 'lib/raspberry-pi/%s/libpv_picollm.so' % _PV_MACHINE)
     elif _PV_SYSTEM == 'Windows':
-        if _PV_MACHINE == 'x86_64':
+        if _PV_MACHINE.lower() == 'amd64':
             return os.path.join(os.path.dirname(__file__), relative, 'lib/windows/amd64/libpv_picollm.dll')
-        elif _PV_MACHINE == "arm64":
+        elif _PV_MACHINE.lower() == 'arm64':
             return os.path.join(os.path.dirname(__file__), relative, 'lib/windows/arm64/libpv_picollm.dll')
 
     raise NotImplementedError('Unsupported platform.')

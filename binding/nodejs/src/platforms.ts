baa21cff8219290c25d3a2622d97ef2489e2f68e
Matthew Maxwell <matthew@picovoice.ai>
2025-01-08 16:19:36 -0800

    minor fix

--- binding/nodejs/src/platforms.ts
+++ binding/nodejs/src/platforms.ts
@@ -129,7 +129,7 @@ export function getPlatform(): string {
     return PLATFORM_MAC;
   }
 
-  if (system === SYSTEM_WINDOWS && (arch === X86_64 || arch == ARM_64)) {
+  if (system === SYSTEM_WINDOWS && (arch === X86_64 || arch === ARM_64)) {
     return PLATFORM_WINDOWS;
   }
 
@@ -180,9 +180,9 @@ export function getSystemLibraryPath(): string {
         break;
       }
       case SYSTEM_WINDOWS: {
-        if (arch === X86_64) {
+        if (arch === X86_64 || arch === ARM_64) {
           return absoluteLibraryPath(
-            SYSTEM_TO_LIBRARY_PATH.get(`${SYSTEM_WINDOWS}/${X86_64}`)
+            SYSTEM_TO_LIBRARY_PATH.get(`${SYSTEM_WINDOWS}/${arch}`)
           );
         }
         break;

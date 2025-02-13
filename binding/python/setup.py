52acad04c9399085606015f069ce3e5625af6de5
Zhuojin Liu <liu@picovoice.ai>
2025-02-12 16:50:49 -0800

    New python SDK and demo version

--- binding/python/setup.py
+++ binding/python/setup.py
@@ -54,7 +54,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
 
 setuptools.setup(
     name="picollm",
-    version="1.2.4",
+    version="1.2.5",
     author="Picovoice",
     author_email="hello@picovoice.ai",
     description="picoLLM Inference Engine",

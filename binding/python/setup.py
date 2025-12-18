da27713e61b8a794cf9a3c3c39b5332df9857c5a
Eric Mikulin <ErisMik@users.noreply.github.com>
2025-12-17 16:56:40 -0800

    v2.0 python (#111)

--- binding/python/setup.py
+++ binding/python/setup.py
@@ -54,7 +54,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
 
 setuptools.setup(
     name="picollm",
-    version="1.3.1",
+    version="2.0.0",
     author="Picovoice",
     author_email="hello@picovoice.ai",
     description="picoLLM Inference Engine",

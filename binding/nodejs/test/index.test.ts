78cdbb1cbdfa03c864ea68df9202482c1f6de568
Gabriel Stang <gabriel@picovoice.ai>
2026-04-24 12:47:33 -0700

    add correct logic to skip ocr large

--- binding/nodejs/test/index.test.ts
+++ binding/nodejs/test/index.test.ts
@@ -716,7 +716,7 @@ describe('PicoLLM generate OCR small', () => {
 });
 
 describe('PicoLLM generate OCR large', () => {
-  test(`generate_ocr_large`, async () => {
+  (process.env.SKIP_OCR_LARGE === "true" ? test.skip : test)(`should be able to generate OCR large`, async () => {
     const data = testData.generate_ocr_large;
     const imagePath = data.image;
     const image = await readImageFile(imagePath);

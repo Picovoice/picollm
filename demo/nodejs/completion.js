cc5dc98578129762662d79ac1af77f4d713727fd
Gabriel Stang <gabriel@picovoice.ai>
2026-04-24 13:52:47 -0700

    update copyright correctly

--- demo/nodejs/completion.js
+++ demo/nodejs/completion.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 //
-// Copyright 2026 Picovoice Inc.
+// Copyright 2024-2026 Picovoice Inc.
 //
 // You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
 // file accompanying this source.

4b6d7afa688c7f45a36ba5ff9cff9db0f9107437
tinapicovoice <tina@picovoice.ai>
2026-05-08 16:40:02 -0700

    v2.1.1 python (#156)

--- binding/python/setup.py
+++ binding/python/setup.py
@@ -54,7 +54,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
 
 setuptools.setup(
     name="picollm",
-    version="2.1.0",
+    version="2.1.1",
     author="Picovoice",
     author_email="hello@picovoice.ai",
     description="picoLLM Inference Engine",
--- demo/python/setup.py
+++ demo/python/setup.py
@@ -16,11 +16,15 @@ shutil.copy(
 shutil.copy(
     os.path.join(os.path.dirname(__file__), 'picollm_demo_completion.py'),
     os.path.join(package_folder, 'picollm_demo_completion.py'))
+shutil.copy(
+    os.path.join(os.path.dirname(__file__), 'picollm_demo_ocr.py'),
+    os.path.join(package_folder, 'picollm_demo_ocr.py'))
 
 with open(os.path.join(os.path.dirname(__file__), 'MANIFEST.in'), 'w') as f:
     f.write('include picollmdemo/LICENSE\n')
     f.write('include picollmdemo/picollm_demo_chat.py\n')
     f.write('include picollmdemo/picollm_demo_completion.py\n')
+    f.write('include picollmdemo/picollm_demo_ocr.py\n')
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
     long_description = f.read()
@@ -30,7 +34,7 @@ with open(os.path.join(os.path.dirname(__file__), "requirements.txt"), "r") as f
 
 setuptools.setup(
     name="picollmdemo",
-    version="2.1.0",
+    version="2.1.1",
     author="Picovoice",
     author_email="hello@picovoice.ai",
     description="picoLLM Inference Engine demos",
